349432644c939dbe2c5890457a3457821d09d64b
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-01-22 13:18:32 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- demos/autoload.php
+++ demos/autoload.php
@@ -5,8 +5,8 @@ date_default_timezone_set('EUROPE/AMSTERDAM');
  * We want to make sure we load the correct autoloader
  * where ever we are.
  */
-if (is_file(__DIR__ . '/../vendor/autoload.php')) {
-    require_once __DIR__ . '/../vendor/autoload.php';
+if (is_file(__DIR__.'/../vendor/autoload.php')) {
+    require_once __DIR__.'/../vendor/autoload.php';
 } else {
-    require_once __DIR__ . '/../../../autoload.php';
+    require_once __DIR__.'/../../../autoload.php';
 }
